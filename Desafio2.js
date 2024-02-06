1979a6b2b236febed8e9a7534c5cec7340dac517
CrisZanutto <criszanutto@hotmail.com>
2024-02-06 09:08:42 -0300

    Desafio2

---
+++ Desafio2.js
@@ -0,0 +1,33 @@
+
+// Função que calcula o nível de um jogador com base nas vitórias e derrotas
+function calcularNivel(vitorias, derrotas) {
+  // Calculando o saldo de Ranqueadas
+  var saldoVitorias = vitorias - derrotas;
+
+  // Determinando o nível com base no saldo de vitórias
+  let nivel;
+
+  if (vitorias < 10) {
+    nivel = "Ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+
+  // Exibindo a mensagem de saída
+  console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+}
+
+// Exemplo de uso da função
+calcularNivel(120, 110); // Substitua os valores de vitórias e derrotas pelos valores desejados
+
+
